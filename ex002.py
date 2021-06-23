18698d59432363fd1422b1508bae7b48d7d159f5
JoeltonLP <joelton.mpto.dev@gmail.com>
2021-06-22 23:49:27 -0400

    Foi implementado o programa
    related #2
    
    closes #2

---
+++ ex002.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+''' Criem um programa que pede um nome de uma pessoa e mostre uma mensagem de 
+boas vinda.'''
+
+name = input('What is your name ? ')
+
+print('Welcome {}' .format(name))
+
+
