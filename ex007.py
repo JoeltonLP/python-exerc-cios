600e5ccaa76d16b7b15182dcfb79216b80dbda67
JoeltonLP <joelton.mpto.dev@gmail.com>
2021-07-01 09:59:07 -0400

    Foi implementado o programa related #10
    closes #10

---
+++ ex007.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre 
+# sua média
+
+test1 = float(input('test1: '))
+test2 = float(input('test: '))
+
+average = (test1 + test2)/2
+
+print('your media is {}' .format(average))
