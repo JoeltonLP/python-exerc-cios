cebe8bedebcf32911bb4eb4075b06e1407069c47
JoeltonLP <joeltonlima@mpto.mp.br>
2021-08-20 17:37:03 -0300

    Implementação do exercicio 14

---
+++ ex0014.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+celsius = float(input('celsius: '))
+converted_value_fahrenheit = ((celsius * 9) / 5) + 32 
+
+print(f' {celsius}° celcius = {converted_value_fahrenheit}° fahrenheit')
